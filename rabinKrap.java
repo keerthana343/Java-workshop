c9da44b2c22fe88af777df684b466f97c908d8ab
Keerthana masetti <keerthanamasetti01@gmail.com>
2026-01-08 22:37:21 +0530

    Add files via upload

---
+++ rabinKrap.java
@@ -0,0 +1,52 @@
+public class rabinKrap {
+    static final int d = 256; // Number of characters in the input alphabet
+    static final int q = 101; // A prime number
+
+    static void search(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+        int i, j;
+        int p = 0; // hash value for pattern
+        int t = 0; // hash value for text
+        int h = 1;
+
+        // The value of h would be "pow(d, M-1)%q"
+        for (i = 0; i < M - 1; i++)
+            h = (h * d) % q;
+
+        // Calculate the hash value of pattern and first window of text
+        for (i = 0; i < M; i++) {
+            p = (d * p + pat.charAt(i)) % q;
+            t = (d * t + txt.charAt(i)) % q;
+        }
+
+        // Slide the pattern over text one by one
+        for (i = 0; i <= N - M; i++) {
+            // Check the hash values of current window of text and pattern.
+            if (p == t) {
+                // Check for characters one by one
+                for (j = 0; j < M; j++) {
+                    if (txt.charAt(i + j) != pat.charAt(j))
+                        break;
+                }
+                if (j == M)
+                    System.out.println("Pattern found at index " + i);
+            }
+
+            // Calculate hash value for next window of text: Remove leading digit,
+            // add trailing digit
+            if (i < N - M) {
+                t = (d * (t - txt.charAt(i) * h) + txt.charAt(i + M)) % q;
+
+                // We might get negative value of t, converting it to positive
+                if (t < 0)
+                    t = (t + q);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String txt = "GEEKS FOR GEEKS";
+        String pat = "GEEK";
+        search(pat, txt);
+    }
+}
