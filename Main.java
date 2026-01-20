e38f5c81b7bca3ab2f2d4ff7444733547f39daf9
Keerthana masetti <keerthanamasetti01@gmail.com>
2026-01-20 15:16:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        short a;
+        short b;
+        short c;
+        a=b=c=10;
+        a++;
+        b+=1;
+        c=(short)(c+1);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
